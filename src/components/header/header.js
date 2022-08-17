9920e6d5eb3cdca37811682ccacb048e5e854168
seyelich <robot11148@gmail.com>
2022-08-17 18:55:39 +0700

    bugfix: некоторые поправки

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,15 +8,15 @@ export default function AppHeader() {
                 <ul className={`${headerStyles.list} default-list`}>
                     <li className={`${headerStyles.item} mr-7 ml-5`}>
                         <BurgerIcon type="primary"/>
-                        <a className="text text_type_main-default ml-2">Конструктор</a>
+                        <a href='#' className="text text_type_main-default ml-2">Конструктор</a>
                     </li>
                     <li className={`${headerStyles.item} mr-7 ml-5`}>
                         <ListIcon type="secondary" />
-                        <a className="text text_type_main-default text_color_inactive ml-2">Лента заказов</a>
+                        <a href='#' className="text text_type_main-default text_color_inactive ml-2">Лента заказов</a>
                     </li>
                     <li className={`${headerStyles.item} mr-5 ml-5`}>
                         <ProfileIcon type="secondary" />
-                        <a className="text text_type_main-default text_color_inactive ml-2">Личный кабинет</a>
+                        <a href='#' className="text text_type_main-default text_color_inactive ml-2">Личный кабинет</a>
                     </li>
                 </ul>
             </nav>
