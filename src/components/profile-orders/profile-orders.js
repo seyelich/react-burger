d6cf65ffba2c5380200937123c66c6301ecfb5da
seyelich <robot11148@gmail.com>
2022-10-29 12:13:16 +0700

    bugfix: fix profile-orders-2

--- src/components/profile-orders/profile-orders.js
+++ src/components/profile-orders/profile-orders.js
@@ -3,13 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { WS_CONNECTION_CLOSED_AUTH, WS_CONNECTION_START_AUTH } from "../../services/actions/ws-auth";
 import { FeedItem } from "../feed-item/feed-item";
 import styles from "./profile-orders.module.css";
-import { useLocation } from 'react-router-dom'
 
 export const ProfileOrders = () => {
     const { ordersInfo } = useSelector(store => store.wsAuth);
     const dispatch = useDispatch();
-    const location = useLocation()
-    console.log({ordersInfo: ordersInfo, location: location})
+
     useEffect(() => {
         dispatch({ type: WS_CONNECTION_START_AUTH });
         return () => dispatch({ type: WS_CONNECTION_CLOSED_AUTH });
@@ -19,7 +17,7 @@ export const ProfileOrders = () => {
         <ul className={`${styles.list} pr-4 ml-15`}>
             {
                 ordersInfo
-                    ? ordersInfo.orders.map(el => <FeedItem hasStatus={true} key={el._id} item={el} />)
+                    ? ordersInfo.orders?.map(el => <FeedItem hasStatus={true} key={el._id} item={el} />)
                     : <p className="loading text text_type_main-large">...</p>
             }
         </ul>
